96547efd2ff4cadf232daaeeee75e101efd70464
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-12-05 15:54:40 +0100

    projrct-set-up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import Button from "react-bootstrap/Button"
+
+function App() {
+  return (
+    <div className="App">
+   <Button>click me</Button>
+    </div>
+  );
+}
+
+export default App;
